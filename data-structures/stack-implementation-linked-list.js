018f223dfe3e6533dab7c9a62aaba08157c87dd2
Milena Shishkova <milena@a-triks.com>
2020-01-14 18:59:56 +0200

    Stack as linked list

---
+++ data-structures/stack-implementation-linked-list.js
@@ -0,0 +1,59 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    peek() {
+        return this.top;
+    }
+    push(value) {
+        let newNode = new Node(value);
+        if(this.length === 0) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const holdingPointer = this.top;
+            this.top = newNode;
+            this.top.next = holdingPointer;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if(!this.top) {
+            return null;
+        }
+        const holdingPointer = this.top;
+        this.top = this.top.next;
+        this.length --;
+        return this;
+    }
+
+    isEmpty() {
+        if (this.length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+// last in first out
+
+
+//Discord
+//Udemy
+//google
+
+const myStack = new Stack();
+myStack.push('Google');
